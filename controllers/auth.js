1eef844db9c347884b8ad6d79237c2191430e144
Carlos <charlyqv98@gmail.com>
2023-11-07 19:45:56 -0600

    Express Validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,20 @@
 const { response } = require('express');
-
+const { validationResult } = require('express-validator');
 
 
 const crearUsuario = (req, res = response ) => {
 
   const { name, email, password } = req.body;
 
-  if ( name.length < 5 ) {
+  const errors = validationResult( req );
+  if ( !errors.isEmpty() ) {
     return res.status(400).json({
       ok: false,
-      msg: 'El nombre debe de ser de 5 letras'
-    })
-  } 
+      errors: errors.mapped()
+    });
+  }
 
-  res.json({
+  res.status(201).json({
     ok: true,
     msg: 'registro',
     name,
@@ -26,7 +27,15 @@ const loginUsuario = (req, res = response ) => {
 
   const { email, password } = req.body;
 
-  res.json({
+  const errors = validationResult( req );
+  if ( !errors.isEmpty() ) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    });
+  }
+
+  res.status(201).json({
     ok: true,
     msg: 'login',
     email,
