410abdfbfc4450ff3b81e781bdc4f7e4830b4113
Carlos <charlyqv98@gmail.com>
2023-11-14 19:21:42 -0600

    Eliminar Evento

--- controllers/events.js
+++ controllers/events.js
@@ -46,7 +46,7 @@ const actualizarEvento = async ( req, res = response ) => {
     const evento = await Evento.findById( eventoId);
 
     if ( !evento ) {
-      res.status(404).json({
+      return res.status(404).json({
         ok: false,
         msg: 'Evento no existe por ese id'
       })
@@ -82,11 +82,41 @@ const actualizarEvento = async ( req, res = response ) => {
 
 }
 
-const eliminarEvento = ( req, res = response ) => {
-  res.json({
-    ok: true,
-    msg: 'eliminarEvento'
-  })
+const eliminarEvento = async ( req, res = response ) => {
+
+  const eventoId = req.params.id;
+  const uid = req.uid;
+
+  try {
+
+    const evento = await Evento.findById( eventoId);
+
+    if ( !evento ) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Evento no existe por ese id'
+      })
+    }
+
+    if ( evento.user.toString() !== uid ) {
+      return res.status(401).json({
+        ok: false,
+        msg: 'No tiene permiso para eliminar este evento'
+      });
+    }
+
+    await Evento.findByIdAndDelete( eventoId );
+
+    res.json({ ok: true });
+    
+  } catch (error) {
+    console.log("🚀 ~ file: events.js:46 ~ actualizarEvento ~ error:", error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    });
+    
+  }
 }
 
 
